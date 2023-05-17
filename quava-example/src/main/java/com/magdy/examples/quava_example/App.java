ac4ca61085df68b54f32ba726885d9021a5309e6
Mohamedmagdy1790 <mmagdy3396@gmail.com>
2023-05-17 11:58:32 +0300

    created

---
+++ quava-example/src/main/java/com/magdy/examples/quava_example/App.java
@@ -0,0 +1,24 @@
+package com.magdy.examples.quava_example;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Multiset<String> animals = HashMultiset.create();
+        
+        animals.add("cat");
+        animals.add("cat");
+
+        animals.add("dog");
+        animals.forEach(System.out::println);
+
+        
+    }
+}
